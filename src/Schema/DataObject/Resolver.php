eebcb080da656f4ea61ebe8602e38e2cb4265c80
Aaron Carlino <unclecheese@leftandmain.com>
2021-03-15 23:22:23 +1300

    Numerous updates to union/interface generation to support Gatsby

--- src/Schema/DataObject/Resolver.php
+++ src/Schema/DataObject/Resolver.php
@@ -26,7 +26,7 @@ class Resolver
      * @return array|bool|int|mixed|DataList|DataObject|DBField|SS_List|string|null
      * @throws SchemaBuilderException
      */
-    public static function resolve($obj, array $args = [], array $context = [], ?ResolveInfo $info = null)
+    public static function resolve($obj, $args = [], $context = [], ?ResolveInfo $info = null)
     {
         $fieldName = $info->fieldName;
         $result = FieldAccessor::singleton()->accessField($obj, $fieldName);
